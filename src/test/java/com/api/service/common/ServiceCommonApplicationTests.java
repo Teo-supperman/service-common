ee91a05249e1eedf390c5e84f5785d2f10d8e729
tobi <tai.mongtri@gmail.com>
2023-04-09 02:46:44 +0700

    init commit

---
+++ src/test/java/com/api/service/common/ServiceCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.service.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
